e11d90837d97fff27283bac3febc5292b3db3360
soorajydv <soorajydv9@gmail.com>
2025-04-03 10:57:56 +0545

    added currency units as constants

---
+++ src/constants/currency.const.js
@@ -0,0 +1,8 @@
+const CURRENCY_UNITS = {
+    usd: 100,  // 100 cents = 1 USD
+    inr: 100,  // 100 paise = 1 INR
+    eur: 100,  // 100 cents = 1 EUR
+    gbp: 100,  // 100 pence = 1 GBP
+};
+
+export default CURRENCY_UNITS;
